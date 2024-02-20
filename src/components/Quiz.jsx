733e5df64bb48e618dd47124ae6989efd25ae183
ngxpaul <phongnx.22git@vku.udn.vn>
2024-02-20 17:30:41 +0700

    first commit

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import QUESTIONS from "../question.js";
+
+export default function Quiz() {
+  // const [activeQuestion,setActiveQuestionIndex] = useState(0);
+
+  const [userAnswers, setUserAnswers] = useState([]);
+
+  const activeQuestionIndex = userAnswers.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswers) => {
+      return [...prevUserAnswers, selectedAnswer];
+    });
+  }
+
+  return (
+    <div>
+      <div className="question">
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
+        <ul id="answers">
+          {QUESTIONS[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className="answer">
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
+}
